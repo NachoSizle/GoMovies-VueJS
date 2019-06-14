12d14489d79b84c8f23911679eafca15e91c85c4
Nacho <developer.nacho@gmail.com>
2019-06-14 13:47:53 +0200

    Firebase integration has been added.

--- src/main.js
+++ src/main.js
@@ -12,6 +12,10 @@ import '@/compiled-icons/igloo-solid';
 import '@/compiled-icons/user-cog-solid';
 import '@/compiled-icons/search-solid';
 
+import firebase from 'firebase';
+import config from '@/settings/firebase-config.js';
+firebase.initializeApp(config);
+
 Vue.config.productionTip = false;
 
 new Vue({
