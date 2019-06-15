31b20397911a2a0d890f2db2b2a9b0ebb5f9c91a
Nacho Martínez <anxooxna@me.com>
2019-06-15 21:14:48 +0200

    Show favorites media in profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,9 +1,25 @@
 import Avatar from '@/components/Avatar/Avatar.vue';
+import { mapActions, mapGetters } from 'vuex';
 
 export default {
   name: 'Profile',
   components: {
     Avatar
+  },
+  data: function () {
+    return {
+      favoritesSelected: 'userMovies'
+    }
+  },
+  computed: {
+    ...mapGetters({
+      userId: 'common/userId',
+      userMovies: 'userMovies/userMovies',
+      userSeries: 'userSeries/userSeries'
+    }),
+    mediaSelected: (state) => {
+      return state[state.favoritesSelected].favorites
+    }
   },
   props: {
     userName: {
@@ -28,5 +44,30 @@ export default {
       type: Number,
       default: '7'
     }
+  },
+  methods: {
+    ...mapActions(
+      'common', [
+        'setSpinnerStatus'
+      ]
+    ),
+    ...mapActions(
+      'userMovies', [
+        'setMovies'
+      ]
+    ),
+    ...mapActions(
+      'userSeries', [
+        'setSeries'
+      ]
+    ),
+    selectFavorites (type) {
+      this.favoritesSelected = type
+    }
+  },
+  created () {
+    let vm = this
+    vm.$fbServices.Movies.getUserMovies(vm, vm.userId)
+    vm.$fbServices.Series.getUserSeries(vm, vm.userId)
   }
 }
