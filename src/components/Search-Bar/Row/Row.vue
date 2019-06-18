971c2ca5bb12cf521217e003c862680e8736ed1f
Nacho Martínez <anxooxna@me.com>
2019-06-18 12:16:03 +0200

    Change search-bar component.

---
+++ src/components/Search-Bar/Row/Row.vue
@@ -0,0 +1,3 @@
+<template src="./Row.pug"></template>
+<script src="./Row.js"></script>
+<style src="./Row.scss" scoped lang="scss"></style>
--- src/components/Search-Bar/Search-Bar.js
+++ src/components/Search-Bar/Search-Bar.js
@@ -1,5 +1,15 @@
+import SearchRow from '@/components/Search-Bar/Row/Row.vue';
+
 export default {
   name: 'Search--Bar',
+  data: () => {
+    return {
+      resultsMedia: []
+    }
+  },
+  components: {
+    SearchRow
+  },
   methods: {
     search () {
       console.debug('SEARCH')
